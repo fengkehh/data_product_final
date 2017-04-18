201bc7d6ea1598813cec4c0541318a33874454d2
fengkehh <fengkehh@gmail.com>
2017-04-18 14:59:40 -0700

    Significant changes to the user interface to allow tooltips.

--- ui.R
+++ ui.R
@@ -1,64 +1,64 @@
 library(shiny)
+library(shinyBS)
 library(plotly)
 library(leaflet)
 
 # Define UI for application
 shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("2016 U.S. Air Pollution Visualizer"),
+  
+  # Sidebar for pollutant & metric selection
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('select_pollutant', 'Select Pollutant', choices = list()),
+      
+      uiOutput('selectUI'),
+      
+      br(),br(),
+      a(href = '', 'More Info'),
+      br(), br(),
+      strong('Created by Keh-Harng Feng')
+      
+    ),
     
-    # Application title
-    titlePanel("2016 U.S. Air Pollution Visualizer"),
     
-    # Sidebar for pollutant & metric selection
-    sidebarLayout(
-        sidebarPanel(
-            
-            selectInput('select_pollutant', label = h3('Select Pollutant'), 
-                        choices = list()),
-            
-            selectInput('select_metric', label = h3('Select Metric'),
-                        choices = list()),
-            
-            br(),br(),
-            strong('Tips:'),
-            div('Select pollutant and corresponding data product metrics for visualization.'),
-            br(),br(),
-            a(href = '', 'More Info'),
-            br(), br(),
-            strong('Created by Keh-Harng Feng')
-            
+    mainPanel(
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"
+      ),
+      tabsetPanel(
+        tabPanel('Monitor Sites', 
+                 leafletOutput('site'),
+                 checkboxInput('top_ten', label = 'Show Top 10', value = FALSE),
+                 bsTooltip('top_ten', 
+                           'Show only the top 10 sites with highest measured concentrations',
+                           placement = 'bottom',
+                           options = list(container = 'body')),
+                 checkboxInput('circle', label = 'Visualize Concentration', value = TRUE),
+                 bsTooltip('circle', 
+                           'Toggle visual display of concentration level',
+                           placement = 'bottom',
+                           options = list(container = 'body')),
+                 actionLink('site_tips', label = 'Tips'),
+                 bsPopover('site_tips', 
+                           'Hover over individual markers to display ranks compared to other markers.<br>Click on markers to zoom (cluster) or display mean (individual)',
+                           trigger = 'click',
+                           placement = 'top')
+                     
         ),
         
-        
-        mainPanel(
-            tags$style(type="text/css",
-                       ".shiny-output-error { visibility: hidden; }",
-                       ".shiny-output-error:before { visibility: hidden; }"
-            ),
-            tabsetPanel(
-                tabPanel('Monitor Sites', 
-                         leafletOutput('site'),
-                         checkboxInput('top_ten', label = 'Show Top 10', value = FALSE),
-                         checkboxInput('circle', label = 'Visualize Concentration', value = TRUE),
-                         p(),
-                         strong('Tips:'),
-                         div('Hover over a marker to see the rank of the site concentration compared to all other displayed markers.',
-                             br(),br(),
-                             'Click on a marker to show concentration level.',
-                             br(),br(),
-                             'Show Top 10 shows the top 10 sites with highest measured concentrations.',
-                             br(),br(),
-                             'Visualize Concentration controls the display of concentration circles around each site.')
-                         ),
-                
-                tabPanel('State Average', plotlyOutput('state'),
-                         p(),
-                         strong('Tips:'),
-                         div('Hover over each state to see corresponding measured values.',
-                         br(),br(),
-                         'Missing popup label means no data is available for the state!')
-                         )
-                
-            )
+        tabPanel('State Average', plotlyOutput('state'),
+                 actionLink('state_tips', label = 'Tips'),
+                 bsPopover('state_tips', 
+                           'Hover over each state to display detailed info about each state.<br>No label means no data available.',
+                           trigger = 'click',
+                           placement = 'top')
         )
+        
+      )
     )
+  )
 ))
